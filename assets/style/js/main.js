9ae64483e2ccd7e0bf8766fc3acf5a442c8e5840
Praisejnr <praiseomoniyi65@gmail.com>
2026-04-07 10:18:00 -0700

    Completed

--- assets/style/js/main.js
+++ assets/style/js/main.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".navBar");
 const modeBtn = document.querySelector(".modeToggleBtn");
+const modal = document.querySelector(".modal_wrapper");
+const acceptCookie = document.getElementById("acceptCookie");
+const declineCookie = document.getElementById("declineCookie");
+const close_btn = document.getElementById("close_btn");
 
 hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
@@ -23,3 +27,23 @@ window.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("dark_theme");
   }
 });
+
+function closeModal() {
+  modal.style.display = "none";
+  document.body.classList.remove("modal_open");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (!localStorage.getItem("cookieAccepted")) {
+    modal.style.display = "flex";
+    document.body.classList.add("modal_open");
+  } else {
+    closeModal();
+  }
+});
+
+acceptCookie.addEventListener("click", () => {
+  localStorage.setItem("cookieAccepted", "true");
+});
+
+  
